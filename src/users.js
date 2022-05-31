0daf040799a7a7c3767ce1beb13e0bfdf247b3ef
Dillion Megida <dillionmegida@gmail.com>
2022-05-31 13:44:47 +0200

    feat: enter placeholder for user token

--- src/users.js
+++ src/users.js
@@ -4,40 +4,35 @@ const users = [
     name: 'Dillion',
     image: 'https://dillionmegida.com/img/deee.jpg',
     bio: 'Just here, doing my thing. Developer advocate at @getstream_io',
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWFtZGlsbGlvbiJ9.e18WIJfIY7Kh85Q8t6PmtjeEikuKmo0fBHSsRhkZd-U',
+    token: 'ENTER TOKEN FOR iamdillion',
   },
   {
     id: 'getstream_io',
     name: 'Stream',
     image: 'https://avatars.githubusercontent.com/u/8597527?s=200&v=4',
     bio: 'Deploy activity feeds and chat at scale with Stream – an API driven platform powering over a billion end users. Get started at http://getstream.io.',
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2V0c3RyZWFtX2lvIn0.T-m5hLaLEamTO6YfGnf1E5yUIuJPXCVcstG9BcjSzE4',
+    token: 'ENTER TOKEN FOR getstream_io',
   },
   {
     id: 'jake',
     name: 'Jake',
     image: 'https://picsum.photos/300/300',
     bio: 'Just Jake, nothing much',
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFrZSJ9.g-_d-cFGQYLfxtsaD6VVC56oyzFcz0264cSuNYzqPL8',
+    token: 'ENTER TOKEN FOR jake',
   },
   {
     id: 'joe',
     name: 'Joe',
     image: 'https://picsum.photos/200/200',
     bio: 'How are you?',
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9lIn0.q_19Ox-40e9uWwJFEumXcME31x8Y8YZuNdgLefVNGF0',
+    token: 'ENTER TOKEN FOR joe',
   },
   {
     id: 'mike',
     name: 'Mike',
     image: 'https://picsum.photos/400/400',
     bio: 'I am mike here. I do things on #react and #javascript',
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlrZSJ9.-to8vHwjxRMJT4scTMYwudK77A-EZekfEyO8JuGHs0k',
+    token: 'ENTER TOKEN FOR mike',
   },
 ]
 
